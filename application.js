d4ca583ebfe059ce9bd3c817b6a027a1e2f9725d
Lindsay Pond <lepond@gmail.com>
2015-02-28 14:11:57 -0800

    Completed all basic functionality, including event actions to toggle to and from main stream and user streams; time stamp works correctly; tweets appear in proper order. Still some code clean up to do + a deep dive into CSS.

--- application.js
+++ application.js
@@ -1,11 +1,12 @@
  $(document).ready(function(){
-        var $body = $('body .mainfeed'),
-            tweetsShown=0;
+        var $mainfeed = $('body .mainfeed'),
+            $userfeed = $('body .userfeed'),
+            tweetsShown = 0;
         
         // broke out tweet message, user, date into separate components
         var tweetConstructor = function (tweet) {
                 var $tweet = $('<div class="tweet-block"></div>'),
-                  $date = $('<div class="date"</div>').text('at' + moment(tweet.created_at).format("h:mm:ss a, dddd, MMMM Do YYYY")),
+                  $date = $('<div class="date"</div>').text('at ' + moment(tweet.created_at).format("h:mm:ss a, dddd, MMMM Do YYYY")),
                   $message = $('<div class="message">').text(tweet.message),
                   $user = $('<div class="user"</div>').text('@' + tweet.user);
                   $message.appendTo($tweet);
@@ -13,27 +14,39 @@
                   $user.prependTo($tweet);
                 return $tweet;
               };
-
-        var tweetAdder = function () {
-        var index = streams.home.length - 1;
-        while (index > tweetsShown){
-          var tweet = streams.home[index];
+// This gathers and displays the tweets of a feed
+        var tweetAdder = function (index, location, src) {
+        var stream = streams['users'][src] || streams['home'];
+        var limit = stream['length'];
+        while (index < limit){
+          var tweet = stream[index];
           var $tweet = tweetConstructor(tweet);
-          $tweet.prependTo($body);
-          tweetsShown++;
-          index--;
+          $tweet.prependTo(location);
+          index++;
           }
         };
-        tweetAdder();
-
-        $('.new-tweets').on('click', tweetAdder);
-      
-
-
-        
-              });
-      
+        tweetAdder(tweetsShown, $mainfeed);
+// This shows new tweets when button is clicked
+        $('.new-tweets').on('click', function () {
+          return tweetAdder(tweetsShown, $mainfeed);});
               
+        var toggleFeed = function () {
+          $('#main').toggleClass('hidden');
+          $('#user').toggleClass('hidden');
+        };
+// This shows a user's tweets when the username is clicked
+        $('.mainfeed').on('click', '.user', function () {
+          toggleFeed();
+          var username = $(this).text().slice(1).toString();
+          return tweetAdder(0, $userfeed, username);
+        });
+// This returns user to main feed and clears the userfeed
+        $('.return-to-feed').on('click', function () {
+          toggleFeed();
+          $userfeed.text("");
+          return tweetAdder(tweetsShown, $mainfeed);
+        });
+          });    
 
 
 
