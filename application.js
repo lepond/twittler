67d167346ec71bc2b5eae69ccfc7a371a7c09620
Lindsay Pond <lepond@gmail.com>
2015-02-28 16:59:04 -0800

    Created a functional version of Twittler with mild styling; complete for now, but hoping to refine style further and do extra credit later.

--- application.js
+++ application.js
@@ -6,12 +6,14 @@
         // broke out tweet message, user, date into separate components
         var tweetConstructor = function (tweet) {
                 var $tweet = $('<div class="tweet-block"></div>'),
-                  $date = $('<div class="date"</div>').text('at ' + moment(tweet.created_at).format("h:mm:ss a, dddd, MMMM Do YYYY")),
-                  $message = $('<div class="message">').text(tweet.message),
-                  $user = $('<div class="user"</div>').text('@' + tweet.user);
-                  $message.appendTo($tweet);
-                  $date.appendTo($tweet);
-                  $user.prependTo($tweet);
+                  $date = $('<div class="date"</div>').html('at ' + moment(tweet.created_at).format("h:mm:ss a, dddd, MMMM Do YYYY")),
+                  $message = $('<div class="message">').html(tweet.message),
+                  $user = $('<div class="user"</div>').html('@' + tweet.user ),
+                  $avatar = $('<div class="avatar"><img src="avatars/' + tweet.user + '.jpg"></div>');
+                $date.prependTo($tweet);
+                $message.prependTo($tweet);
+                $user.prependTo($tweet);
+                $avatar.prependTo($tweet);
                 return $tweet;
               };
 // This gathers and displays the tweets of a feed
@@ -37,7 +39,13 @@
 // This shows a user's tweets when the username is clicked
         $('.mainfeed').on('click', '.user', function () {
           toggleFeed();
-          var username = $(this).text().slice(1).toString();
+          var username = $(this).text().slice(1);
+          return tweetAdder(0, $userfeed, username);
+        });
+        $('.mainfeed').on('click', '.avatar', function () {
+          toggleFeed();
+          var username = $(this).closest('.tweet-block').find('.user').text().slice(1);
+          console.log(username);
           return tweetAdder(0, $userfeed, username);
         });
 // This returns user to main feed and clears the userfeed
